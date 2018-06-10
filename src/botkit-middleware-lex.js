981c5c86680370a9b654bc2ade487f64918f2ee8
marcotrinelli <30631770+marcotrinelli@users.noreply.github.com>
2018-06-10 17:25:55 +0200

    Removed hardcoded region and fixed code

--- src/botkit-middleware-lex.js
+++ src/botkit-middleware-lex.js
@@ -1,12 +1,22 @@
 var LexRuntime = require('aws-sdk').LexRuntime
-var lex = new LexRuntime({
-    region: 'eu-west-1'
-})
-
 
 module.exports = function(config) {
-
+    
+    var lex = new LexRuntime({
+        region: config.region
+    })
+    
     function receive(bot, message, next) {
+    
+        if (!message.text) {
+          next();
+          return;
+        }
+
+        if (message.is_echo || message.type === 'self_message') {
+          next();
+          return;
+        } 
         var params = {
             botAlias: config.botAlias,
             botName: config.botName,
